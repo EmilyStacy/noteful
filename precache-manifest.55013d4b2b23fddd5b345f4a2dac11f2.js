a15184d44d8486b838bdfd977accfc82245f30d5
Emily Stacy <beyonce@thinkful.com>
2019-07-06 16:27:05 -0500

    Updates

---
+++ precache-manifest.55013d4b2b23fddd5b345f4a2dac11f2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "969eaed9a0ea8d4fdf7404f5fc3ae032",
+    "url": "/noteful/index.html"
+  },
+  {
+    "revision": "56c52653842f45a31d9a",
+    "url": "/noteful/static/css/main.a872b198.chunk.css"
+  },
+  {
+    "revision": "a746c57dcb9bb0a7a139",
+    "url": "/noteful/static/js/2.23827abc.chunk.js"
+  },
+  {
+    "revision": "56c52653842f45a31d9a",
+    "url": "/noteful/static/js/main.e1331834.chunk.js"
+  },
+  {
+    "revision": "9b20a66fc41c82b5fb41",
+    "url": "/noteful/static/js/runtime~main.b82ff86d.js"
+  }
+]);
